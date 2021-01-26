75324e5a2787272c024aca995d0c204a5c06b481
KSFlynn007 <kelseyflynnn@gmail.com>
2021-01-26 15:46:29 -0700

    Update main-view.jsx

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -1,26 +1,26 @@
 import React from 'react';
 import axios from 'axios';
 
-class MainView extends React.Component{
-    constructor(){
-        super();
-        // initalize empty object state to be destructured later
-        // so it can be accessed later by const { something } = this.state
-        this.state = {};
-    }
+// class MainView extends React.Component{
+//     constructor(){
+//         super();
+//         // initalize empty object state to be destructured later
+//         // so it can be accessed later by const { something } = this.state
+//         this.state = {};
+//     }
 
-    render() {
-        return(
-            <div className="main-view"></div>
-        );
-    }
-}
+//     render() {
+//         return(
+//             <div className="main-view"></div>
+//         );
+//     }
+// }
 
 export class MainView extends React.Component {
     componentDidMount() {
         axios.get('https://m-y-f-l-i-x.herokuapp.com/movies')
             .then (response => {
-                // never directly mutate state once defined, otherwise component won't update:
+                // never directly mutate state once defined, otherwise component won't update
                 this.setState({
                     movies: response.data
                 });
