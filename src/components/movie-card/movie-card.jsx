e88754f4772e788138aabe6590fdba510b370b6e
KSFlynn007 <kelseyflynnn@gmail.com>
2021-01-29 16:03:46 -0700

    Update movie-card.jsx
    import and use PropTypes for verification for MovieCard entries

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 export class MovieCard extends React.Component {
     render() {
@@ -9,3 +10,26 @@ export class MovieCard extends React.Component {
         );
     }
 }
+
+MovieCard.propTypes = {
+    // shape({...}) means it expects an object
+    movie: PropTypes.shape({
+        // movie prop may contain Title, and IF it does, it must be a string
+        Title: PropTypes.string.isRequired,
+        Description: PropTypes.string.isRequired,
+        Year: PropTypes.string.isRequired,
+        ImageURL: PropTypes.string.isRequired,
+        Genre: PropTypes.shape({
+            Name: PropTypes.string.isRequired,
+           Biography: PropTypes.string.isRequired 
+        }),
+        Director: PropTypes.shape({
+            Name: PropTypes.string.isRequired,
+            Bio: PropTypes.string.isRequred,
+            Birthdate: PropTypes.instanceOf(Date).isRequired
+        }),
+        Featured: PropTypes.bool
+    }).isRequired,
+    // props object must contain onClick and it MUST be a function
+    onClick: PropTypes.func.isRequred
+};
