74124ecd9a7055374d0deedebac9473f91ffcff6
KSFlynn007 <kelseyflynnn@gmail.com>
2021-01-29 18:47:27 -0700

    Update login-view.jsx
    swapped basic return to bootstrap return form, added proptypes validation

--- src/components/login-view/login-view.jsx
+++ src/components/login-view/login-view.jsx
@@ -1,4 +1,9 @@
 import React, {useState} from 'react';
+import PropTypes from 'prop-types';
+
+import './login-view.scss';
+
+import { Form, Button} from 'react-bootstrap';
 
 export function LoginView(props) {
     const [ username, setUsername ] = useState('');
@@ -11,17 +16,49 @@ export function LoginView(props) {
         props.onLoggedIn(username);
     };
 
-    return(
-        <form>
-            <label>
-                Username: 
-                <input type="text" value={username} onChange={e => setUsername(e.target.value)} />
-            </label>
-            <label>
-                Password:
-                <input type="password" value={password} onChange={e => setPassword(e.target.value)} />
-            </label>
-            <button type="button" onClick={handleSubmit}>Submit</button>
-        </form>
-    );
+  return (
+    <React.Fragment>
+      <Form className='form-login'>
+        <h1 className='text-danger'>Welcome to myFlix!</h1>
+        <p className='mb-5'>Please login to continue.</p>
+        <Form.Group controlId='formBasicEmail'>
+          <Form.Label>Username</Form.Label>
+          <Form.Control
+            type='text'
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+            placeholder='Enter Username'
+          />
+        </Form.Group>
+        <Form.Group controlId='formBasicPassword'>
+          <Form.Label>Password</Form.Label>
+          <Form.Control
+            type='password'
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            type='password'
+            placeholder='Enter Password'
+          />
+        </Form.Group>
+        {/*<p>
+          Dont have an account?
+          <Button onClick={handleRegister} variant='link'>
+            Register
+          </Button>
+        </p>*/}
+        <Button onClick={handleSubmit} variant='primary' type='submit'>
+          Submit
+        </Button>
+      </Form>
+    </React.Fragment>
+  );
 }
+
+LoginView.propTypes = {
+    user: PropTypes.shape({
+        username: PropTypes.string.isRequired,
+        pasword: PropTypes.string.isRequired
+    }),
+    onLoggedIn: PropTypes.func.isRequired,
+    onRegister: PropTypes.func,
+};
