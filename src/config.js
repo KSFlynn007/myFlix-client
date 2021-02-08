24df9fbe43a3889edbad22d741981087f2d6e41a
KSFlynn007 <kelseyflynnn@gmail.com>
2021-02-08 13:15:43 -0700

    Update config.js
    added string -> date config to use in DirectorView

--- src/config.js
+++ src/config.js
@@ -2,5 +2,8 @@
 
 export default {
     // API_URL: 'http://localhost:8080',
-    API_URL: 'https://m-y-f-l-i-x.herokuapp.com'
+    API_URL: 'https://m-y-f-l-i-x.herokuapp.com',
+
+    MONTHS: ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
 }
+
