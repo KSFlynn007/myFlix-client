76e39cd1ba7dd3384c7c04e333c22a442fd7cc57
KSFlynn007 <kelseyflynnn@gmail.com>
2021-02-11 12:42:22 -0700

    Create reducers.js

---
+++ src/reducers/reducers.js
@@ -0,0 +1,39 @@
+import {combineReducers} from 'redux';
+
+import { SET_FILTER, SET_MOVIES, SET_USER } from '../actions/actions';
+
+function visibilityFilter (state = '', action) {
+    switch (action.Type) {
+        case SET_FILTER:
+            return action.value;
+        default: 
+            return state;
+    }
+}
+
+function movies(state = [], action) {
+    switch (action.type) {
+        case SET_MOVIES:
+            return action.value;
+        default: 
+            return state;
+    }
+}
+
+function users(state= '', action) {
+    switch (action.type) {
+        case SET_USER:
+            return action.value;
+        default:
+            return state;
+    }
+}
+
+// groups all reducers together and passes the state they're concerned with - filter for the first reducers and movies for the second one
+const moviesApp = combineReducers({
+    visibilityFilter,
+    movies,
+    users
+});
+
+export default moviesApp; 
