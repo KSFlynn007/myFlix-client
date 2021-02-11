a4d4428af49edfd86cfecb51117fa28c275ba6ba
KSFlynn007 <kelseyflynnn@gmail.com>
2021-02-11 16:36:57 -0700

    Visibility typos/changes

--- src/components/visibility-filter-input/visibility-filter-input.jsx
+++ src/components/visibility-filter-input/visibility-filter-input.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
+import './visibility-filter-input.scss';
 
 import Form from 'react-bootstrap/Form';
 
@@ -10,7 +10,8 @@ function VisibilityFilterInput(props) {
     return <Form.Control
         onChange={e => props.setFilter(e.target.value)}
         value={props.visibilityFilter}
-        placeholder='filter'
+        placeholder='Search'
+        className='form-control-search'
     />;
 }
 
