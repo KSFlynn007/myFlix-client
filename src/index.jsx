619587af172cb9b547745178af2af2f606cf1849
KSFlynn007 <kelseyflynnn@gmail.com>
2021-02-11 11:46:31 -0700

    Update index.jsx
    initialize store in overall application

--- src/index.jsx
+++ src/index.jsx
@@ -1,15 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+
 import { MainView } from './components/main-view/main-view';
+import moviesApp from './reducers/reducers';
 
 // commands that we bundle ./index.scss
 import './index.scss';
+
+// only ever 1 store
+const store = createStore(moviesApp);
 
 // main component
 class MyFlixApplication extends React.Component {
   render() {
     return (
-      <MainView />
+      <Provider store={store}>
+        <MainView />
+      </Provider>
     );
   }
 }
