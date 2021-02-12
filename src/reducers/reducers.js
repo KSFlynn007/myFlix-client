6270f04a566f088c2d0f885b6d901727c5e76d22
KSFlynn007 <kelseyflynnn@gmail.com>
2021-02-12 13:22:22 -0700

    Update reducers.js
    Capitalized t in Type which was causing the entire search bar issue

--- src/reducers/reducers.js
+++ src/reducers/reducers.js
@@ -3,7 +3,7 @@ import {combineReducers} from 'redux';
 import { SET_FILTER, SET_MOVIES, SET_USER } from '../actions/actions';
 
 function visibilityFilter (state = '', action) {
-    switch (action.Type) {
+    switch (action.type) {
         case SET_FILTER:
             return action.value;
         default: 
